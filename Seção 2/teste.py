14780c9001f7cd4c86773972b15ad7b28283b865
felipefalleiros <164528635+felipefalleiros@users.noreply.github.com>
2024-04-29 12:26:02 -0300

    Delete Seção 2 directory

--- Seção 2/teste.py
+++
@@ -1,6 +0,0 @@
-def inverter_dicionario(dicionario):
-    return {valor: chave for chave, valor in dicionario.items()}
-
-# Exemplo de uso:
-dicionario = {'a': 1, 'b': 2, 'c': 3}
-print(inverter_dicionario(dicionario))
