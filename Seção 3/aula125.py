04ee9938fb56a8a40f907b05d333045b6f5669e6
Felipe Falleiros <mendesf84@gmail.com>
2024-05-16 15:25:07 -0300

    atributos da classe

---
+++ Seção 3/aula125.py
@@ -0,0 +1,17 @@
+class Pessoa:
+    # atributo da classe que pode ser acessado por todos os objetos
+    ano_atual = 2022
+
+    def __init__(self, nome, idade) -> None:
+        self.nome = nome
+        self.idade = idade
+
+    def get_ano_nascimento(self):
+        return Pessoa.ano_atual - self.idade
+   
+p1 = Pessoa('João', 35)
+p2 = Pessoa('Helena', 12)
+print(Pessoa.ano_atual)
+
+print(p1.get_ano_nascimento())
+print(p2.get_ano_nascimento())
